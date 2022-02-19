127755bd90d68c2d4ccdcccb5e9594e301662fd0
Kushal Jaiswal <jaiswalk@student.unimelb.edu.au>
2022-02-19 16:24:46 +1100

    Fix: uploading the correct version of the social media background image

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -41,14 +41,14 @@ const HomePage = () => {
               className="mt4 flex justify-center center bg-no-repeat bg-center mb5"
             >
               <img
-                style={{ width: "15%", height: "90%" }}
+                style={{ width: "35%", height: "90%" }}
                 className="mt5"
                 src={Left}
                 alt={"Left"}
               />
-              <img style={{ width: "20%" }} src={Middle} alt={"Middle"} />
+              <img style={{ width: "40%" }} src={Middle} alt={"Middle"} />
               <img
-                style={{ width: "15%", height: "90%" }}
+                style={{ width: "35%", height: "90%" }}
                 className="mt5"
                 src={Right}
                 alt={"Right"}
